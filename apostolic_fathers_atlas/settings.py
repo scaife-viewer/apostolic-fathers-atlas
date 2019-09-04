94cb8fa7f6df16ce80d0281526669659b633ffe7
Jacob Wegner <jacobwegner@gmail.com>
2019-09-04 06:20:50 -0500

    enable whitenoise and corsheaders middlewares

--- apostolic_fathers_atlas/settings.py
+++ apostolic_fathers_atlas/settings.py
@@ -103,7 +103,9 @@ TEMPLATES = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
